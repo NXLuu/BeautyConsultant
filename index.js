aecd8a8eceb1d2efd57caf06f858103ad92a8b12
Luu Nguyen <nxuluu@gmail.com>
2022-01-12 15:32:56 +0700

    Upload heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 let faceRoute = require('./routes/face.route');
 let bodyRoute = require('./routes/body.route');
